53d0825ed967835623ccb43cf25f1014174c7b2f
mtbehisseste <monkey860315@gmail.com>
2020-01-20 22:26:36 +0800

    Update installation

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+vim +PluginInstall +qall
+
 # -----YouCompleteMe-----
 sudo apt install build-essential cmake python3-dev
 # This installs python lint only but auto complete for other languages also works 
 # If semantic support for C-family languages is needed, run this:
-# cd ~/.vim/bundle/YouCompleteMe && python3 install.py --clang-completer
-cd ~/.vim/bundle/YouCompleteMe && python3 install.py  
+# cd bundle/YouCompleteMe && python3 install.py --clang-completer
+cd bundle/YouCompleteMe && python3 install.py  
 
 # Check https://github.com/ycm-core/YouCompleteMe for more 
 # -----YouCompleteMe-----
 
-vim +PluginInstall +qall

c6cf89344dfc50b57ed4652c3fb4e61d3acd61de
mtbehisseste <monkey860315@gmail.com>
2020-02-05 13:41:55 +0800

    Update readme and install.sh

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ A mix-up of many plugins and wierd settings.
 - [markdown-preview](https://github.com/iamcco/markdown-preview.vim) 
 
 ## Installation (Requires `sudo` while installing prerequests)
-You might want to backup your `.vim/` and `.vimrc` if they exists, and remove them before installing.
+You might want to backup your `.vim/` and `.vimrc` if they exist, and remove them before installing.
 ```bash 
 $ git clone --recursive https://github.com/mtbehisseste/.vim.git ~/.vim
 $ cd ~/.vim
--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ sudo apt install build-essential cmake python3-dev
 # This installs python lint only but auto complete for other languages also works 
 # If semantic support for C-family languages is needed, run this:
 # cd bundle/YouCompleteMe && python3 install.py --clang-completer
-cd bundle/YouCompleteMe && python3 install.py --clang-completer
+cd bundle/YouCompleteMe && python3 install.py
 
-# Check https://github.com/ycm-core/YouCompleteMe for more 
+# Check https://github.com/ycm-core/YouCompleteMe for more information
 # -----YouCompleteMe-----
 
 echo "source ~/.vim/vimrc" > ~/.vimrc
